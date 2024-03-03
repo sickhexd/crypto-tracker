4ad9c41eaaa24f3b4fd5b94c384647956415f3d9
sickhexd <sabiaflex0@gmail.com>
2024-03-03 22:46:08 +0700

    added firts botton of user menu

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main(page: ft.Page):
     page.window_width = 650
     page.window_height = 450
 
-
+    
     
     user_output = ft.Text(value='')
     user_output_table = ft.Text(value='')
@@ -37,12 +37,28 @@ def main(page: ft.Page):
             on_change=lambda _: update_output()
             )
 
+    row1 = ft.Row([input1, input2], alignment=ft.MainAxisAlignment.CENTER)
+    row2 = ft.Row([user_output], alignment=ft.MainAxisAlignment.CENTER)
+    row3 = ft.Row([user_output_table], alignment=ft.MainAxisAlignment.CENTER)
+
+    rows_added = False
+
+    def button_clicked(e):
+        nonlocal rows_added
+        if not rows_added:
+            page.add(row1, row2, row3)
+            rows_added = True
+        else:
+            page.remove(row1, row2, row3)
+            rows_added = False
+   
+            
+
+    trades_bottom = ft.TextButton('Trades', on_click= button_clicked, data=0)
+
+
     def update_output():
         nonlocal user_output, user_output_table
-        # user_output.value = func.get_trades(input1.value, input2.value)
-        # user_output.update()
-        # user_output_table.value = func.data_framed(input1.value, input2.value)
-        # user_output_table.update()
         if input1.value == input2.value:
             user_output.value = 'error'
             user_output_table.value = ''
@@ -53,24 +69,7 @@ def main(page: ft.Page):
         user_output_table.update()
 
     page.add(
-        ft.Row(
-            [
-                input1,input2
-            ],
-            alignment=ft.MainAxisAlignment.CENTER
-        ),
-        ft.Row(
-            [
-                user_output
-            ],
-            alignment=ft.MainAxisAlignment.CENTER
-        ), 
-        ft.Row(
-            [
-                user_output_table
-            ],
-            alignment=ft.MainAxisAlignment.CENTER
-        )
-        )
+        ft.Row([trades_bottom],alignment=ft.MainAxisAlignment.CENTER),
+    )
 
 ft.app(target=main)
